b8567eddf49df18c7a570672831b578a89df842e
xhuanlee <lixhuan822@163.com>
2020-12-04 17:21:46 +0800

    change peer to https://t.callt.net

--- config.js
+++ config.js
@@ -9,9 +9,9 @@ const admin_account = '0xD227AF0e36AE44e673b0143d7765Dc4dA9B64B68';
 
 module.exports = {
   network_id: '1515',
-  ethereum_rpc_endpoint: 'http://gfax.f3322.org:7545',
-  swarm_http_endpoint: 'http://gfax.f3322.org:8500',
-  api_http_endpoint: 'http://111.231.58.73:4000',
+  ethereum_rpc_endpoint: 'https://t.callt.net/eth',
+  swarm_http_endpoint: 'https://t.callt.net/swarm',
+  api_http_endpoint: 'https://t.callt.net/api',
 
   admin: {
     token_admin: admin_account,
